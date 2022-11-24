641de2205960b5b1bcc8ead4622213269692804a
Thunderbird93 <antircristian@gmail.com>
2022-11-24 12:47:36 +0100

    [add] components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+import HeaderApp from './components/HeaderApp.vue'
+import MainApp from './components/MainApp.vue'
+import FooterApp from './components/FooterApp.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    HeaderApp,
+    MainApp,
+    FooterApp
+  }
 }
 </script>
 
 <template>
-  
+  <HeaderApp />
+  <MainApp />
+  <FooterApp />
 </template>
 
 <style lang="scss" scoped>
