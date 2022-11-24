be96026883a7f1209f2443f6ec80b6b35bf46e6c
Thunderbird93 <antircristian@gmail.com>
2022-11-24 14:51:42 +0100

    [add] container to components

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,13 +1,16 @@
 <script>
 export default {
-
+    name: 'FooterApp'
 }
 </script>
 
 <template>
-  
+  <div class="container"></div>
 </template>
 
 <style lang="scss" scoped>
+@use '../style/general.scss';
+@use '../style/partials/mixin' as *;
+@use '../style/partials/variables' as *;
 
 </style>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,18 @@
 <script>
 export default {
-
+    name: 'HeaderApp'
 }
 </script>
 
 <template>
-  
+  <header>
+    <div class="container"></div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../style/general.scss';
+@use '../style/partials/mixin' as *;
+@use '../style/partials/variables' as *;
 
 </style>
