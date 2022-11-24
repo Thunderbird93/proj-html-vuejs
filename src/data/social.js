02f6dc587aaebde401b6ac9a7265faef4c7c89bf
Thunderbird93 <antircristian@gmail.com>
2022-11-24 17:57:33 +0100

    [fix] nomi per facilità d'uso

--- src/data/social.js
+++ src/data/social.js
@@ -1,23 +1,23 @@
 export default [
     {
         nome: 'facebook',
-        icon: '<i class="fa-brands fa-facebook-f"></i>',
+        img: '<i class="fa-brands fa-facebook-f"></i>',
         path: 'https://www.facebook.com/search/top/?q=boolean.careers'
     },
     {
         nome: 'twitter',
-        icon: '<i class="fa-brands fa-twitter"></i>',
+        img: '<i class="fa-brands fa-twitter"></i>',
         path: 'https://twitter.com/booleancareers'
     },
     {
         nome: 'instagram',
-        icon: '<i class="fa-brands fa-instagram"></i>',
-        path: 'https://www.instagram.com/boolean.careers/'
+        img: '<i class="fa-brands fa-instagram"></i>',
+        path: 'https://www.instagram.com/boolean.careers/',
     },
     {
         nome: 'youtube',
-        icon: '<i class="fa-brands fa-youtube"></i>',
+        img: '<i class="fa-brands fa-youtube"></i>',
         path: 'https://www.youtube.com/@BooleanCareers'
-    }
+    },
 ]
 
