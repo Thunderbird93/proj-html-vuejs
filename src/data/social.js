3970f77da109aab09cd37719f4c3ae58c3fa58d3
Thunderbird93 <antircristian@gmail.com>
2022-11-24 16:52:54 +0100

    [add] file per le icone social

---
+++ src/data/social.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        nome: 'facebook',
+        icon: '<i class="fa-brands fa-facebook-f"></i>',
+        path: 'https://www.facebook.com/search/top/?q=boolean.careers'
+    },
+    {
+        nome: 'twitter',
+        icon: '<i class="fa-brands fa-twitter"></i>',
+        path: 'https://twitter.com/booleancareers'
+    },
+    {
+        nome: 'instagram',
+        icon: '<i class="fa-brands fa-instagram"></i>',
+        path: 'https://www.instagram.com/boolean.careers/'
+    },
+    {
+        nome: 'youtube',
+        icon: '<i class="fa-brands fa-youtube"></i>',
+        path: 'https://www.youtube.com/@BooleanCareers'
+    }
+]
+
