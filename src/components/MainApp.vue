97e101ff14f61ef61dd8fcdab2178a9b044e1b41
Thunderbird93 <antircristian@gmail.com>
2022-11-25 23:01:34 +0100

    [add] main jumbotron

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -6,11 +6,24 @@ export default {
 
 <template>
     <main>
+      <!-- CTA -->
       <div class="cta">
         <span>
           <i class="fa-regular fa-paper-plane"></i>
           <h5>GET A FREE QUOTE ONLINE NOW!</h5>
         </span>
+      </div>
+      <!-- Jumbotron (Main) -->
+      <div class="main-jumbotorn">
+        <div class="jumbotron">
+          <h6>HEADACHE FREE</h6>
+          <h2 class="display-4">Award Winning</h2>
+          <h2 class="display-4">Moving Company</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore tenetur quia quas veritatis ullam illum voluptatem ad eligendi repellendus facilis sunt quisquam at, error reprehenderit iusto accusamus atque non alias illo, dolores laboriosam consequatur?</p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">READ OUR TESTIMONIALS</a>
+        </p>
+</div>
       </div>
     </main>
 </template>
@@ -28,6 +41,9 @@ main{
     span{
       display: flex;
     }
+  }
+  .main-jumbotorn{
+    background-color: $mc-mercury;
   }
 }
 
