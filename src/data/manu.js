24d7a2a7463f28d7b6f732626393eb27a79a3159
Thunderbird93 <antircristian@gmail.com>
2022-11-25 16:53:31 +0100

    [add] file menu js per nav

---
+++ src/data/manu.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        nome:'Home',
+        path:'#'
+    },
+    {
+        nome:'Rates',
+        path:'#'
+    },
+    {
+        nome:'Testimonials',
+        path:'#'
+    },
+    {
+        nome:'FAQ',
+        path:'#'
+    },
+    {
+        nome:'Blog',
+        path:'#'
+    },
+    {
+        nome:'Contact',
+        path:'#'
+    },
+]
