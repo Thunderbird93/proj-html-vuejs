e3b742e1202435c2b6b7c214bc9dd57b7c757728
Thunderbird93 <antircristian@gmail.com>
2022-11-26 16:02:38 +0100

    [add] top & bottom footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -9,12 +9,38 @@ export default {
 </script>
 
 <template>
-  <div class="container"></div>
+  <footer>
+    <div class="mid-line"></div>
+    <!-- Footer Top -->
+    <div class="footer-top">
+      <div class="ac-container"></div>
+    </div>
+    <!-- /Footer Top -->
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+      <div class="ac-container"></div>
+    </div>
+    <!-- /Footer Bottom -->
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/general.scss';
 @use '../style/partials/mixin' as *;
 @use '../style/partials/variables' as *;
+
+footer{
+  .mid-line{
+    height: 20px;
+    background-color: $cc-lima;
+  }
+  .footer-top{
+    height: 300px;
+  }
+  .footer-bottom{
+    height: 200px;
+    background-color: $cc-lima;
+  }
+}
 
 </style>
