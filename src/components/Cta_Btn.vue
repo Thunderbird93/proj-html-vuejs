9917a94494d3f59c280cf6d50cb8ddb5e2f4aedb
Thunderbird93 <antircristian@gmail.com>
2022-11-27 23:50:15 +0100

    [add] btn component

---
+++ src/components/Cta_Btn.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'Cta_Btn'
+}
+</script>
+
+<template>
+  <span>FREE QUOTE</span>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss';
+@use '../style/partials/mixin' as *;
+@use '../style/partials/variables' as *;
+
+span{
+    @include cta_btn;
+    @include center;
+}
+
+</style>
